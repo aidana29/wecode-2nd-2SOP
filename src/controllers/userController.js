617fa08c914cf0d44929e0b880f3c3325c6efcbe
dana <aidana1529@gmail.com>
2023-09-21 14:47:07 +0900

    feat: 회원가입 userController/userDao 구현

---
+++ src/controllers/userController.js
@@ -0,0 +1,16 @@
+const userService = require("../services/userService");
+
+const signUp = async(req, res) => {
+
+    const { lastName, firstName, email, password } = req.body;
+
+    if (!lastName || !firstName || !email || !password){
+        const error = new Error("KEY_ERROR")
+        error.status = 400
+        throw error
+    };
+
+    await userService.signUp(lastName, firstName, email, password);
+
+    res.status(201).json({ message: "USER_CREATED" });
+}
---
+++ src/services/userService.js
@@ -0,0 +1,14 @@
+const userDao = require("../models/userDao");
+
+const signUp = (lastName, firstName, email, password) => {
+
+    // 비밀번호 number, 소문자, 대문자, 특수문자, 5자 이상
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*).$/
+
+    if (!passwordRegex.test(password)) {
+        const error = new Error ("NO_CHARACTERS")
+        error.status = 400
+        throw error
+    }
+
+}
