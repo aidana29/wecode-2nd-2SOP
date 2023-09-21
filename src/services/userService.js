89e8b544faf79ee63f937127eaf1ae7c8205b1b5
dana <aidana1529@gmail.com>
2023-09-21 17:21:11 +0900

    feat: 회원가입 구현 및 테스트 완료

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,17 @@
 const userDao = require("../models/userDao");
 
-const signUp = (lastName, firstName, email, password) => {
+const signUp = async (lastName, firstName, email, password, phoneNumber) => {
 
     // 비밀번호 number, 소문자, 대문자, 특수문자, 5자 이상
     const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*).$/
 
-    if (!passwordRegex.test(password)) {
-        const error = new Error ("NO_CHARACTERS")
-        error.status = 400
-        throw error
-    }
+    // if (!passwordRegex.test(password)) {
+    //     const error = new Error ("NO_CHARACTERS")
+    //     error.status = 400
+    //     throw error
+    // }
 
+    await userDao.signUp(lastName, firstName, email, password, phoneNumber);
 }
+
+module.exports = { signUp }
