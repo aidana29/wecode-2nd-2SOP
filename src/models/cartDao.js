fdc81136e76ee948ebf45a984d47d957eaba1102
leehoyoung <ihoyeong215@gmail.com>
2023-09-26 21:32:17 +0900

    수정ing

--- src/models/cartDao.js
+++ src/models/cartDao.js
@@ -1,17 +1,12 @@
 const { DataSource } = require("typeorm");
 
-const myDataSource = new DataSource({
-    type: process.env.TYPEORM_TYPE,
-    host: process.env.TYPEORM_HOST,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DB,
-    port: process.env.TYPEORM_PORT,
-})
+
 const findCartIndex = async(user_id) =>{`
     SELECT id
     FROM user_id = ${user_id}
 `}
+return userId;
+
 const addInCart = async () => {
     await myDataSource.query(`
     INSERT INTO carts (user_id, status) VALUE
@@ -19,8 +14,10 @@ const addInCart = async () => {
     `)
     const cart_id = findCartIndex(req.user_id)
     await myDataSource.query(`
+    INSERT INTO cart_items (product_id,cart_is,price,quntity) VALUE
+    (${req.body.productId}, ${cart_id}, ${price} 1);
     
-    `)
+    `);
 
 }
 
@@ -39,24 +36,27 @@ const showCart = async () => {
 const exProductsDao = async (userId, productId) => {
     try{
         await myDataSource.query(`
-        SELETE * FROM cart WHERE user_id =???, product_id = ??`, [user_id, product_id])
+        SELETE * FROM cart WHERE user_id =???, product_id = ??`, [user_id, product_id]
+        );
     } catch (err) {
-        const error = new Error("Error Dao")
-        error.statusCode = 500
-        throw error
+        const error = new Error("Error Dao");
+        error.statusCode = 500;
+        throw error;
     }
-}
+};
 
 const deleteCartsDao = async (productId) => {
     try {
         await myDataSource.query(`
-        SELETE FROM cart WHERE product_id =??` , [productId])
+        SELETE FROM cart WHERE product_id =??` , [productId]
+        );
     } catch (err) {
-        const error = new Error ("Error Dao")
-        error.statusCode = 500
-        throw error
+        const error = new Error ("Error Dao");
+        error.statusCode = 500;
+        throw error;
     }
-}
+};
 
 
 
+module.exports = {findCartIndex, showCart, exProductsDao, deleteCartsDao};
