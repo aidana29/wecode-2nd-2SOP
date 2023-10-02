e297190c457858ffb83c2f437f2fe171c44fc5e1
JunwooPark <wnsdn1994@gmail.com>
2023-10-03 01:53:35 +0900

    fix : cart 기능 구현

--- src/models/cartDao.js
+++ src/models/cartDao.js
@@ -1,28 +1,28 @@
 const { myDataSource } = require("./dataSource");
 
-const findCartIndex = async(user_id) =>{
-    const userId = await myDataSource.query(`
+const findCartIndex = async (user_id) => {
+  const userId = await myDataSource.query(`
     SELECT id
     FROM user_id = ${user_id}
 `);
-return userId;
+  return userId;
 };
 
 const addInCart = async (userId, productId, price) => {
-    await myDataSource.query(`
+  await myDataSource.query(`
     INSERT INTO carts (user_id) VALUE
     (${userId});
-    `)
-    const cart_id = findCartIndex(req.user_id);
-    await myDataSource.query(`
+    `);
+  const cart_id = findCartIndex(req.user_id);
+  await myDataSource.query(`
     INSERT INTO cart_items (product_id,cart_is,price,quntity) VALUE
     (${productId}, ${cart_id}, ${price}, 1);
     
     `);
-}
+};
 
 const showCart = async () => {
-    await myDataSource.query(`
+  await myDataSource.query(`
     SELECT 
     cart_items.cart_id,
     cart_items.price,
@@ -30,21 +30,19 @@ const showCart = async () => {
     cart_items.product_id,
     cart.id FROM
     cart.id = cart_items.cart_id;
-    `)
-}
-
-const deleteCartsDao = async (productId) => {
-//     try {
-//         await myDataSource.query(`
-//         SELETE FROM cart WHERE product_id =??` , [productId]
-//         );
-//     } catch (err) {
-//         const error = new Error ("Error Dao");
-//         error.statusCode = 500;
-//         throw error;
-//     }
+    `);
 };
 
+const deleteCartsDao = async (productId) => {
+  //     try {
+  //         await myDataSource.query(`
+  //         SELETE FROM cart WHERE product_id =??` , [productId]
+  //         );
+  //     } catch (err) {
+  //         const error = new Error ("Error Dao");
+  //         error.statusCode = 500;
+  //         throw error;
+  //     }
+};
 
-
-module.exports = {findCartIndex, showCart, exProductsDao, deleteCartsDao};
+module.exports = { findCartIndex, showCart, addInCart, deleteCartsDao };
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -8,8 +8,9 @@ const { cartDao } = require("../models");
 //   console.log("hi");
 // };
 const cartItem = async (productId, selectIndex, quantity) => {
+  //카트id가 user_id와 일치하는지
   const exProducts = await cartDao.findCartIndex(userId);
-  await cartDao.cartItem(productId, exProducts, quantity);
+  await cartDao.addInCart(productId, exProducts, quantity);
 };
 
 const deleteCartsDao = async (productId) => {
