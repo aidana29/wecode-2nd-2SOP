4a59610da975781c947e6182c9495ba843378d7a
JunwooPark <wnsdn1994@gmail.com>
2023-10-02 10:39:32 +0900

    feat: throwError 사용방법

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -4,7 +4,7 @@ const { throwError } = require("../utilities/throwError");
 const orderPayment = async (userId, orderId, address) => {
   //address 비어있음 and 적합하지 않음
   if (!address) {
-    throwError(400, "ADDRESS_ERROR");
+    throwError(400, "ADDRESS_ERROR"); //throwError 사용 방법 예시
   }
   //cart의 주인이 user가 아닌 경우
   //userId 가지고 cartId 구할 수 있는 쿼리문
