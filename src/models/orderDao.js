e6d605007bcc03dba93848a74755fa3541fb85cf
dana <aidana1529@gmail.com>
2023-10-03 23:03:45 +0900

    feat: order 파일 생성 및 구현 시작

---
+++ src/models/orderDao.js
@@ -0,0 +1,35 @@
+const { myDataSource } = require("./dataSource");
+
+const orderToDb = async (userId, productId) => {
+  await myDataSource.query(
+    `INSERT INTO order (user_id, product_id) VALUES ?, ?`,
+    [userId, cartId]
+  );
+};
+
+const shipmentToDb = async (
+  shipmentDate,
+  address,
+  city,
+  state,
+  country,
+  zipCode,
+  userId,
+  orderId
+) => {
+  await myDataSource.query(
+    `INSERT INTO shipment (shipment_date, address, city, state, country, zip_code, user_id, order_id) VALUES ?, ?, ?, ?, ?, ?, ?, ?`,
+    [shipmentDate, address, city, state, country, zipCode, userId, orderId]
+  );
+};
+
+const showOrderItems = async (cartId) => {
+  const orderItems = await myDataSource.query(`SELECT product_id, cart_id, price, quntity FROM cart_items WHERE cart_id = ?`, [cartId]);
+  return orderItems
+};
+
+module.exports = {
+  orderToDb,
+  shipmentToDb,
+  showOrderItems,
+};
---
+++ src/services/orderService.js
@@ -0,0 +1,23 @@
+const orderDao = require("../models/orderDao");
+
+const orderCheck = async (userId, cartId, shipmentDate, address, city, state, country, zipCode) => {
+
+  if (!shipmentDate || !address || !city || !state || !country || !zipCode) {
+    const error = new Error("SHIPMENT_NOT_FOUND");
+    error.status = 400;
+    throw error;
+  }
+
+  await orderDao.orderToDb(userId, cartId);
+  await orderDao.shipmentToDb(shipmentDate, address, city, state, country, zipCode, userId, orderId)
+};
+
+const orderItems = async (cartId) => {
+  const orderItems = await orderDao.showOrderItems(cartId);
+  return orderItems
+}
+
+module.exports = {
+    orderCheck,
+    orderItems
+};
