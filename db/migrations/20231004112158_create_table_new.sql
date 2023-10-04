566a35762ffc39d2d2477f75f7d9a4b6a0bbb8f8
JunwooPark <wnsdn1994@gmail.com>
2023-10-04 20:51:31 +0900

    fix: dbmate 최종본

--- db/migrations/20231004112158_create_table_new.sql
+++ db/migrations/20231004112158_create_table_new.sql
@@ -3,10 +3,9 @@ CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `email` VARCHAR(255) NOT NULL ,
   `password` VARCHAR(255) NOT NULL,
-  `lastname` VARCHAR(255) NOT NULL,
-  `firstname` VARCHAR(255) NOT NULL,
-  `phone_number` VARCHAR(255) NOT NULL,
-  `birth_day` TIMESTAMP NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `first_name` VARCHAR(255) NOT NULL,
+  `birthday` TIMESTAMP NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
@@ -18,12 +17,12 @@ CREATE TABLE `1_category` (
 
 CREATE TABLE `2_category` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  `1_categorty_id` INT NOT NULL,
+  `1_category_id` INT NOT NULL,
   `name` VARCHAR(255)
 );
 
-CREATE TABLE `product` (
-  `id` INT NOT NULL PRIMARY KEY,
+CREATE TABLE `products` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255),
   `description` VARCHAR(255),
   `price` INT NOT NULL,
@@ -33,7 +32,7 @@ CREATE TABLE `product` (
 );
 
 CREATE TABLE `product_info` (
-  `id` INT NOT NULL PRIMARY KEY,
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `suited_to` VARCHAR(255),
   `aroma` VARCHAR(255),
   `key_ingredients` VARCHAR(255),
@@ -43,31 +42,30 @@ CREATE TABLE `product_info` (
 
 CREATE TABLE `carts` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `user_id` INT,
-  `status` INT,
+  `user_id` INT NOT NULL,
+  `status` INT NOT NULL DEFAULT 0,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `cart_items` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `product_id` INT NULL,
-  `cart_id` INT NULL,
-  `price` INT NULL,
-  `quntity` INT NULL
+  `product_id` INT NOT NULL,
+  `cart_id` INT NOT NULL,
+  `price` INT DEFAULT NULL,
+  `quantity` INT DEFAULT NULL
 );
 
-CREATE TABLE `order` (
+CREATE TABLE `orders` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   `user_id` INT NOT NULL,
-  `order_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
-  `content` VARCHAR(255),
+  `cart_id` INT NOT NULL,
+  `content` VARCHAR(255) DEFAULT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `discount` (
+CREATE TABLE `discounts` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   `product_id` INT NOT NULL,
   `dc_percent` INT NOT NULL,
@@ -83,26 +81,25 @@ CREATE TABLE `product_image` (
   `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `shipment` (
+CREATE TABLE `shipments` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
   `shipment_date` DATE,
-  `address` VARCHAR(255),
-  `city` VARCHAR(255),
-  `state` VARCHAR(255),
-  `country` VARCHAR(255),
-  `zip_code` VARCHAR(255),
+  `address` VARCHAR(255) DEFAULT NULL,
+  `city` VARCHAR(255) DEFAULT NULL,
+  `state` VARCHAR(255) DEFAULT NULL,
+  `country` VARCHAR(255) DEFAULT NULL,
+  `zip_code` VARCHAR(255) DEFAULT NULL,
   `user_id` INT NOT NULL,
   `order_id` INT NOT NULL
 );
 
 CREATE TABLE `payments` (
-  `id` INT NOT NULL PRIMARY KEY,
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
   `order_id` INT NOT NULL,
-  `address` VARCHAR(255),
+  `address` VARCHAR(255) DEFAULT NULL,
   `payment_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `payment_method` VARCHAR(255)
+  `payment_method` VARCHAR(255) DEFAULT NULL
 );
 
 -- migrate:down
-
