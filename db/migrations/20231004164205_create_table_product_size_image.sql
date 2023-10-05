3ca34190da19181c81f0718a64841c056d292c71
Jungyeon Laura Kim <jylaurakim@gmail.com>
2023-10-05 12:11:00 +0900

    Update 20231004164205_create_table_product_size_image.sql

--- db/migrations/20231004164205_create_table_product_size_image.sql
+++ db/migrations/20231004164205_create_table_product_size_image.sql
@@ -2,9 +2,9 @@
 CREATE TABLE PRODUCT_SIZE_IMAGE (   --새로운 테이블 생성
     id INT NOT NULL AUTO_INCREMENT,
     product_id INT,
-    product_size VARCHAR(255),--size is a reserved word
-    product_option VARCHAR(255),--option is a reserved word
-    price INT,   --decimal로 해야하나?//JY: 필요없을듯. INT로.
+    product_size VARCHAR(255),
+    product_option VARCHAR(255),
+    price INT,   
     product_image VARCHAR(3000) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES products(id)
